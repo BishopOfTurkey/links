0e745d5232f6f6ee10bbd39a5b70fdb8345fd90d
Obi Symons <obi.symons@anu.edu.au>
2021-04-13 19:15:51 +1000

    Heroku test

--- cmd/linkstore/main.go
+++ cmd/linkstore/main.go
@@ -3,19 +3,27 @@ package main
 import (
 	"flag"
 	"linkstore"
+	"log"
 	"os"
 )
 
 var (
-	pword *string = flag.String("code", "", "Code used verify identity")
-	file  *string = flag.String("file", "links.csv", "CSV to store links in.")
+// pword *string = flag.String("code", "", "Code used verify identity")
+// file  *string = flag.String("file", "links.csv", "CSV to store links in.")
 )
 
 func main() {
 	flag.Parse()
-	if *pword == "" {
-		flag.Usage()
-		os.Exit(1)
+
+	pword := os.Getenv("ACCESS_CODE")
+	if pword != "" {
+		log.Fatalln("Must set an ACCESS_CODE")
 	}
-	linkstore.Server(*pword, *file)
+
+	file := os.Getenv("LINKS_FILE")
+	if file != "" {
+		file = "links.csv"
+	}
+
+	linkstore.Server(pword, file)
 }
